062483f29edd70bc95b469558aa2a58e8d453f2f
Molix <molixrawdi@gmail.com>
2025-09-27 02:21:31 +0100

    test

---
+++ DO/python_radius_radius.py
@@ -0,0 +1,31 @@
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, value):
+        if value <= 0:
+            raise ValueError("Radius must be positive!")
+        self._radius = value
+
+
+#####
+
+
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, value):
+        if value <= 0:
+            raise ValueError("Radius must be positive!")
+        self._radius = value
